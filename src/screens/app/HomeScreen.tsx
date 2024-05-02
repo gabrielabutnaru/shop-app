8f4cd307e31f2c832083a9f1af737a64ccafed51
gabrielabutnaru <gabrielabutnaru24@gmail.com>
2024-05-02 13:36:27 +0300

    Integrate login

--- src/screens/app/HomeScreen.tsx
+++ src/screens/app/HomeScreen.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 export function HomeScreen() {
+  const [userName, setUsername] = useState('');
+  useEffect(() => {
+    axios
+      .get<{ username: string }>('/user/me')
+      .then(({ data: { username } }) => {
+        setUsername(username);
+      })
+      .catch(console.log);
+  }, []);
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Text>{userName}</Text>
     </View>
   );
 }
